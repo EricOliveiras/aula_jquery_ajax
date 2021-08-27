86eff8cd5b4d8e5a14b0770671faa4c1886a4c14
EricOlv <88443797+EricOliveiras@users.noreply.github.com>
2021-08-27 13:47:17 -0300

    Terminado (por enqaunto)

--- js/main.js
+++ js/main.js
@@ -2,15 +2,22 @@
 function consultaCep() {
 
     const cep = document.getElementById('cep').value
-    const url = 'https://viacep.com.br/ws/' + cep  + '/json/'
+    const url = `https://viacep.com.br/ws/${cep}/json/`
 
     $.ajax({
         url: url,
         type: 'GET',
         success: function(response) {
-            console.log(response)
+            $('#uf').html(response.uf)
+            $('#localidade').html(response.localidade)
+            $('#bairro').html(response.bairro)
             $('#logradouro').html(response.logradouro)
+            $('#titulo_cep').html(`CEP : ${response.cep}`)
+            $('.cep').show()
         }
     })
-
 }
+
+$(function() {
+    $('.cep').hide()
+})
